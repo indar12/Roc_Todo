9c2517e27b86a42f3255f7cba7672e4ca5bec31b
Indrajith <indrajithmohanasundaram@gmail.com>
2023-01-20 13:27:43 +0530

    displayed task manually using useState and using map to render the task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./TodoItem";
 
 function Todo() {
+  const [todoItems, setTodoItems] = useState([
+    { taskName: "Sleep well" },
+    { taskName: "Drink water" },
+  ]);
   return (
     <div>
       <h1>TodoList</h1>
       <input type="text" placeholder="Enter the task" />
-      <button type="submit">Submit</button>
+      <button type="submit">Add</button>
       <br></br>
-      <TodoItem itemOne={"Sleep well"}/>
+      <div>
+        {todoItems.map(({taskName})=>{
+            return <TodoItem taskName={taskName}/>
+        })}
+      </div>
+
     </div>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-function TodoItem({itemOne}) {
+function TodoItem({taskName}) {
   return (
     <div className="TodoItems">
-      <li>{itemOne}</li>
-      <li>Exercise daily</li>
+      <li>{taskName}</li>
     </div>
   );
 }
